693d7399cfa312f9a2e2c7ca270b73c742159ed9
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-15 16:46:28 +0000

    fixed function

--- todo-list.py
+++ todo-list.py
@@ -7,22 +7,49 @@ def Remove(todo, idx):
     del todo[idx]
     return todo
 
-# def Update(todo, ):
-
-
-# def Checking(state):
-
+def Update(todo, idx):
+    title = input('qanday nomga o\'zgartirmoqchisiz? ')
+    state = input('taskni holatini kiriting bajarilgan bolsa True aks holda False --> ')
+    if state.lower() == 'true':
+        state = True
+    else:
+        state = False
+    todo[idx].update({"task-name": title, "task-state": state})
+    return todo
+    
+def Checking(todo, idx):
+    if todo[idx]['task-state'] == True:
+        print('task bajarilgan')
+    else:
+        print('task bajarilmagan')
 
 def Create_list(todo):
-    create_task = dict()
-    text = 'javascript'
-    todo.append(Add(create_task, False, text))
-    idx = 0
-    todo = Remove(todo, idx)
+    add_q = input('task qo\'shasizmi? ')
+    if add_q.lower() == 'ha':
+        empty_dict = dict()
+        task_name = input('task nomini kiriting --> ')
+        task_state = input('task holatini kiriting bajarilgan bolsa True aks holda False --> ')
+        if task_state.lower() == 'true':
+            task_state = True
+        else:
+            task_state = False
+        todo.append(Add(empty_dict, task_state, task_name))
+    
+    remove_q = input('taskni o\'chirishni hohlaysizmi? ')
+    if remove_q.lower() == 'ha':
+        idx = int(input('qaysi indexdagi taskni? '))
+        todo = Remove(todo, idx)
+
+    update_q = input('taskni update qilishni hohlaysizmi? ')
+    if update_q.lower() == 'ha':
+        idx = int(input('qaysi indexdagi taski update qimoqchisiz? '))
+        todo = Update(todo, idx)
+
+    checking_q = input('taskni checking qilishni hohlaysizmi? ')
+    if checking_q == 'ha':
+        idx = int(input('qaysi indexdagi taskni checking qimoqchisiz? '))
+        Checking(todo, idx)
+
     print(todo)
-    # Update()
-    # Checking(state)
-# def Clear(todo)
-# def Count(state)
-todo = list()
-Create_list(todo)
+lst = list()
+Create_list(lst)

af14ce516975653ba53dcf8b5eb6326f4bc3f3a8
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-15 12:42:01 +0000

    use def

---
+++ todo-list.py
@@ -0,0 +1,31 @@
+def Add(create_task, state, text):
+    create_task['task-name'] = text
+    create_task['task-state'] = state
+    return create_task
+
+
+def Remove(todo, idx):
+    del todo[idx]
+    return todo
+
+
+# def Update(todo, ):
+
+    # def Checking(state):
+
+
+def Create_list(todo):
+    create_task = dict()
+    text = 'javascript'
+    todo.append(Add(create_task, False, text))
+    idx = 0
+    todo = Remove(todo, idx)
+    print(todo)
+
+
+    # Update()
+    # Checking(state)
+# def Clear(todo)
+# def Count(state)
+todo = list()
+Create_list(todo)
